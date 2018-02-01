f59271567e6f3fde64d01a4087d628fdaf54756f
Kannupriya <kannupriya14@gmail.com>
2018-02-01 18:15:16 +0530

    spychat first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print "Let\'s get started"
+spy_name = input("What is your name?")
+spy_salutation = input("What do you prefer to write(Mr. or Ms.)?")
+spy_name = spy_salutation + spy_name
+print "Alright" + spy_name + ".Let me know more about you"
